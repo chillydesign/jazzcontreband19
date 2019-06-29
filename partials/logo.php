8c5c676a1af89692bf2cde2217d4db6197a5b963
Charles Harvey <harvey.charles@gmail.com>
2019-06-29 12:31:13 +0100

    add top stripes png

--- partials/logo.php
+++ partials/logo.php
@@ -1,5 +1,5 @@
 <a href="<?php echo home_url(); ?>" title="Jazzcontreband" class="branding">
-<span class="logo_jcb">Jazzcontreband</span> -->
+<span class="logo_jcb">Jazzcontreband</span>
     <!-- <span class="logo_jazz">jazz</span>
     <span class="logo_contre">contre</span>
     <span class="logo_band">band</span> -->
